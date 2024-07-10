d97ee9a26be9bf8575628b1978faee1d428a579f
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-07-10 16:12:16 +0300

    анимации not-polished
    Масштабирование и демасштабирование при наведении на книжку
    Глобальные переменные для легковесных анимация типа Tween в коде
    Анимация масштабирования всего окна с фольклорами при увеличении

--- scenes/category.gd
+++ scenes/category.gd
@@ -36,7 +36,7 @@ func _init_counter(in_category: DBElements.CategoryType) -> void:
 		var sources: Array[String] = DBElements.get_folklores_elements_sources(folklore_db_name)
 		for source: String in sources:
 			var tmp_category: DBElements.CategoryType =  DBElements.get_element_category(source)
-			if(_category == tmp_category):
+			if(in_category == tmp_category):
 				_folklores_to_merged+=1
 
 func _on_merged(folklore_db_name: String) -> void:
--- scenes/guidebook.gd
+++ scenes/guidebook.gd
@@ -3,14 +3,35 @@ extends CanvasLayer
 
 @onready var _exit: Button = %Exit
 @onready var _folklore_list: VBoxContainer = %FolkloreList
+@onready var _folklore_window_animations: AnimationPlayer = %FolkloreWindowAnimations
 var _folklore_pc: PackedScene = preload("res://scenes/guidebook_folklore.tscn")
 
 func _ready() -> void:
-	_exit.pressed.connect(queue_free)
+	_connect_signals()
 	_fill_folklore_list()
+	_folklore_window_animations.play("scale")
+
+func _connect_signals() -> void:
+	_exit.pressed.connect(_on_pressed_exit)
+	_folklore_window_animations.animation_started.connect(_on_animation_started)
+	_folklore_window_animations.animation_finished.connect(_on_animation_finished)
 
 func _fill_folklore_list() -> void:
 	for key: String in DBElements._folklores.keys():
 		var folklore_instance: GuidebookFolklore = _folklore_pc.instantiate() as GuidebookFolklore
 		_folklore_list.add_child(folklore_instance)
 		folklore_instance.get_parametres(key)
+
+func _on_animation_started(_anim_name: StringName) -> void:
+	_exit.process_mode = Node.PROCESS_MODE_DISABLED
+	_folklore_list.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _on_animation_finished(_anim_name: StringName) -> void:
+	_exit.process_mode = Node.PROCESS_MODE_ALWAYS
+	_folklore_list.process_mode = Node.PROCESS_MODE_INHERIT
+
+func _on_pressed_exit() -> void:
+	_folklore_window_animations.play("descale")
+	await _folklore_window_animations.animation_finished
+	queue_free()
+
