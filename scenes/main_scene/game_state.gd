1b5c9b049bcdce9b43ddbe33676f37a7a0e85878
iobda <raulichkas@inbox.ru>
2024-07-23 12:52:41 +0200

    Анимация удачного слияния.
    Первая версия. Анимация вызывается через ноду merger, которая находится на главной сцене невидимая.

--- scenes/main_scene/game_state.gd
+++ scenes/main_scene/game_state.gd
@@ -13,11 +13,13 @@ var _popup_folklore_pc: PackedScene = preload("res://scenes/pop_folklore.tscn")
 
 @onready var debug_left: Label = %DebugLeft
 @onready var debug_right: Label = %DebugRight
+@onready var merger: Control = %Merger
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Events.category_closed.connect(_on_category_closed)
 	Events.element_chosen.connect(_on_element_chosen)
+	Events.spawn_popup.connect(_spawn_popup_folklore)
+	merger.visible = false
 
 func _on_category_closed(is_right: bool)->void:
 	if(is_right):
@@ -28,8 +30,10 @@ func _on_category_closed(is_right: bool)->void:
 func _on_element_chosen(element_db_name: String ,is_right: bool)->void:
 	if(is_right):
 		_right_element_selected = element_db_name
+		Events.right_element.emit(element_db_name)
 	else:
 		_left_element_selected = element_db_name
+		Events.left_element.emit(element_db_name)
 	if(_right_element_selected != "none" and _left_element_selected != "none"):
 		_process_merge()
 		_left_element_selected = "none"
@@ -39,7 +43,8 @@ func _process_merge() -> void:
 	var folkore_bd_name: String = DBElements.get_folklore_element_name_by_sources(_left_element_selected,_right_element_selected)
 	if(folkore_bd_name != "none" and folkore_bd_name != "invalid_sources" and DBElements.is_folklore_element_closed(folkore_bd_name)):
 		DBElements.set_folklore_element_to_open(folkore_bd_name)
-		_spawn_popup_folklore(folkore_bd_name)
+		#_spawn_popup_folklore(folkore_bd_name)
+		merger.visible = true
 		Events.merged.emit(folkore_bd_name)
 	else:
 		Events.merge_failed.emit()
---
+++ scenes/merger.gd
@@ -0,0 +1,29 @@
+extends Control
+
+var _right_element_selected: String
+var _left_element_selected: String
+
+@onready var _animation_player: AnimationPlayer = %AnimationPlayer
+@onready var _left_element: Element = %LeftElement
+@onready var _right_element: Element = %RightElement
+
+func _ready() -> void:
+	Events.merged.connect(_animation_merge)
+	Events.right_element.connect(set_right_element)
+	Events.left_element.connect(set_left_element)
+
+func set_right_element(element_db_name: String)->void:
+	_right_element_selected = element_db_name
+	_right_element.set_element_data(_right_element_selected)
+
+func set_left_element(element_db_name: String)->void:
+	_left_element_selected = element_db_name
+	_left_element.set_element_data(_left_element_selected)
+
+func _animation_merge(folkore_bd_name: String) -> void:
+	_animation_player.play("Merge")
+	await _animation_player.animation_finished
+	spawn_popup(folkore_bd_name)
+
+func spawn_popup(folkore_bd_name: String) -> void:
+	Events.spawn_popup.emit(folkore_bd_name)
