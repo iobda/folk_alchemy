f82a3329b90cdf5b98acba4b1bd704777294b0b9
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-07-25 15:15:43 +0300

    commit before sync with main

--- scenes/guidebook.gd
+++ scenes/guidebook.gd
@@ -1,15 +1,35 @@
 class_name GuideBook
 extends CanvasLayer
 
+@export var _guide_book_folklores: Array[GuidebookFolklore] 
+
+var _current_page: int = 0
+
 @onready var _exit: TextureButton = %Exit
-var _folklore_pc: PackedScene = preload("res://scenes/guidebook_folklore.tscn")
+@onready var _prev_page: TextureButton = %PrevPage
+@onready var _next_page: TextureButton = %NextPage
 
 func _ready() -> void:
+	_init_guidebook()
 	_connect_signals()
 
-func _connect_signals() -> void:
-	_exit.pressed.connect(_on_pressed_exit)
+func _init_guidebook() -> void:
+	_current_page = (get_tree().current_scene as GameState).last_current_page
 
-func _on_pressed_exit() -> void:
+func _connect_signals() -> void:
+	_exit.pressed.connect(_on_exit_pressed)
+	_prev_page.pressed.connect(_on_prev_page_pressed)
+	_next_page.pressed.connect(_on_next_page_pressed)
+
+func _update_page() -> void:
+	pass
+
+func _on_exit_pressed() -> void:
 	queue_free()
+
+func _on_next_page_pressed() -> void:
+	pass
+
+func _on_prev_page_pressed() -> void:
+	pass
 
