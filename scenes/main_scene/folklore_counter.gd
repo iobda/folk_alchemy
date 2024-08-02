1805f46786a007afce43f2fdedd143caac334d93
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-08-02 16:34:11 +0300

    Изменение счётчика фольклоров + изменения UI
    Изменение UI подсказок и изменения расположения справки

--- scenes/main_scene/folklore_counter.gd
+++ scenes/main_scene/folklore_counter.gd
@@ -9,11 +9,11 @@ var _counter_text: String = "none"
 
 func _ready() -> void:
 	Events.merged.connect(_on_merged)
-	folklore_counter.text = str(_counter_opened) + " / " + str(_all_folk_count)
+	folklore_counter.text = str(_counter_opened) + " Фольклоров из " + str(_all_folk_count)
 
 func _on_merged(folklore_db_name: String) -> void:
 	_check_state = DBElements.get_folklore_state(folklore_db_name)
 	if _check_state == "opened":
 		_counter_opened += 1
-	_counter_text = str(_counter_opened) + " / " + str(_all_folk_count)
+	_counter_text = str(_counter_opened) + " Фольклоров из "  + str(_all_folk_count)
 	folklore_counter.text = _counter_text
