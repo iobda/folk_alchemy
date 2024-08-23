68fb765f3b1bab4f264a6cc1536441edadc92a3d
iobda <raulichkas@inbox.ru>
2024-08-23 15:04:14 +0200

    обновление sdk_yandex

--- addons/yandex_sdk/sdk_export_plugin.gd
+++ addons/yandex_sdk/sdk_export_plugin.gd
@@ -16,18 +16,25 @@ func _export_begin(features: PackedStringArray, is_debug: bool, path: String, fl
 		exporting = true
 		export_path = path
 
+
 func _export_end() -> void:
 	if exporting:
 		var file := FileAccess.open(export_path, FileAccess.READ)
 		var html := file.get_as_text()
 		file.close()
 		var pos = html.find('</head>')
-		html = html.insert(pos, '<script src="' + JS_SDK_REF + '"></script>\n' + '<script src="' + JS_FILE + '"></script>\n')
+		
+		html = html.insert(pos, 
+				'<script src="' + JS_SDK_REF + '"></script>\n' +
+				'<script src="' + JS_FILE + '"></script>\n')
 		DirAccess.copy_absolute(plugin_path + '/' + JS_FILE, export_path.get_base_dir() + '/' + JS_FILE)
+		
 		file = FileAccess.open(export_path, FileAccess.WRITE)
 		file.store_string(html)
 		file.close()
+
 	exporting = false
+
 
 func _export_file(path: String, type: String, features: PackedStringArray) -> void:
 	if path.begins_with(plugin_path) and not path.ends_with("yandex_sdk.gd"):
