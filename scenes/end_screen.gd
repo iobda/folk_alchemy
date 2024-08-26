803b3e4cc5516037d603737a061d6aa98645ff77
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-08-26 06:06:59 +0300

    ending fix

--- scenes/end_screen.gd
+++ scenes/end_screen.gd
@@ -1,14 +1,8 @@
 extends CanvasLayer
 
 @onready var _ending_texture_rect: TextureRect = %EndingTextureRect
+@onready var _timer: Timer = %Timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	_connect_signals()
-
-func _connect_signals() -> void:
-	await get_tree().create_timer(1.5).timeout
-	_ending_texture_rect.gui_input.connect(_on_gui_input)
-
-func _on_gui_input(in_input_event: InputEvent) -> void:
-	queue_free()
+func _input(event: InputEvent) -> void:
+	if(_timer.is_stopped()):
+		queue_free()
